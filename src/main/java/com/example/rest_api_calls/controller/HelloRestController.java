a4faa1f77ec8b3fa9aa1b950895ce64d707ff5a6
AkshataHasabi <akshuh818@gmail.com>
2022-05-21 20:39:59 +0530

    ADDED:ability to print hello from bridgelab message

--- src/main/java/com/example/rest_api_calls/controller/HelloRestController.java
+++ src/main/java/com/example/rest_api_calls/controller/HelloRestController.java
@@ -1,4 +1,13 @@
 package com.example.rest_api_calls.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloRestController {
+    @RequestMapping(value = {"", "/" }, method = RequestMethod.GET)
+    public String sayHello() {
+        return "Hello from Bridgelabz";
+    }
 }
