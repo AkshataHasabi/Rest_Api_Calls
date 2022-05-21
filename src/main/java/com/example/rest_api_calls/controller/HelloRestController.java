faa2a07a99ee7930317b90d236b2456f21ff2433
AkshataHasabi <akshuh818@gmail.com>
2022-05-21 22:56:14 +0530

    ADDED:ability to pass name parameter  as Query parameter

--- src/main/java/com/example/rest_api_calls/controller/HelloRestController.java
+++ src/main/java/com/example/rest_api_calls/controller/HelloRestController.java
@@ -1,13 +1,21 @@
 package com.example.rest_api_calls.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class HelloRestController {
     @RequestMapping(value = {"", "/" }, method = RequestMethod.GET)
     public String sayHello() {
         return "Hello from Bridgelabz";
+    }
+
+    @RequestMapping(value = {"", "/queryparam" }, method = RequestMethod.GET)
+    public String sayHelloMessage(@RequestParam (value = "name") String name) {
+        return "Hello"+name+ "from Bridgelabz";
+    }
+
+    @GetMapping("/query")
+    public String sayHelloParam(@RequestParam String name) {
+        return "Hello "+name+" From BridgeLabz";
     }
 }
